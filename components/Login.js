e9a4a1da7dc2014c19fd9f46d623d9da0caa853d
David Medero <dmedero@getwrecked.com>
2022-03-25 01:08:01 -0400

    suceesfully added email provider and enhanced UI

--- components/Login.js
+++ components/Login.js
@@ -1,14 +1,45 @@
 import { signIn } from "next-auth/react"
+import { useState } from 'react'
+import { getCsrfToken } from "next-auth/react"
+import Link from 'next/link'
 
 
-export default function Login() {
+export default function Login({ csrfToken, onClose }) {
+
+  const [value, setValue] = useState("")
+
 
       return (
         <div>
           <form class="px-6 space-y-6 lg:px-8 pb-8">
       <h3 class="pb-7 pt-3 text-xl font-bold text-gray-900 dark:text-white text-center">Logo</h3>
-      <button onClick={() => signIn('google')} type="button" class="h-[45px] transition-all ease-in-out duration-600 w-full text-white bg-pink-400 hover:bg-pink-500 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800">Sign in with Google</button>
+
+      <form method="post" action="/api/auth/signin/email">
+      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+      <input value={value} onChange={(e) => setValue(e.target.value)}className="focus:border-pink-300 hover:border-pink-300 h-[45px] border pl-2 mt-3 mb-6 w-full font-medium rounded-lg py-2.5" id="email" type="text" name="email" placeholder="email@example.com" />
+      <Link href="/verify-request" passHref>
+      <a>
+      <button onClick={() => { onClose(); signIn("email", { email: value, redirect: false }) }} type="submit" className="h-[45px] transition-all ease-in-out duration-600 w-full text-white bg-pink-400 hover:bg-pink-500 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800">Sign in with Email</button>
+      </a>
+      </Link>
+      
+      </form>
+
+
+      <div className="divider">Or</div>
+        
+      <button onClick={() => signIn('google')} type="button" className="h-[45px] transition-all ease-in-out duration-600 w-full text-white bg-pink-400 hover:bg-pink-500 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800">Sign in with Google</button>
   </form>
+  
         </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context),
+    }
+  }
+}
+
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,24 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email"
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from './lib/mongodb'
 
 
 export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
+        EmailProvider({
+            server: {
+              host: process.env.EMAIL_SERVER_HOST,
+              port: process.env.EMAIL_SERVER_PORT,
+              auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD
+              }
+            },
+            from: process.env.EMAIL_FROM
+          }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -24,13 +39,13 @@ export default NextAuth({
         async jwt({ token, account }) {
             // Persist the OAuth access_token to the token right after signin
             if (account) {
-              token.accessToken = account.access_token
+              token?.accessToken = account?.access_token
             }
             return token
           },
         async session({ session, token }) {
             // Send properties to the client, like an access_token from a provider.
-            session.accessToken = token.accessToken
+            session?.accessToken = token?.accessToken
             return session
             },
         async redirect({ url, baseUrl }) {
