8e17774d8ebb9c6d5f5509c654ead31200685f00
David Medero <dmedero@getwrecked.com>
2022-03-25 13:34:23 -0400

    successfully added custom email template functionality

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,71 @@ import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import clientPromise from './lib/mongodb'
+import nodemailer from "nodemailer"
 
+
+async function customVerificationRequest({
+  identifier: email,
+  url,
+  provider: { server, from },
+}) {
+  const { host } = new URL(url)
+  const transport = nodemailer.createTransport(server)
+  await transport.sendMail({
+    to: email,
+    from,
+    subject: `Sign in to Bunni Luna`,
+    text: text({ url, host }),
+    html: html({ url, host, email }),
+  })
+}
+
+function html({ url, host, email }) {
+
+  const backgroundColor = "#f9f9f9"
+  const textColor = "#444444"
+  const mainBackgroundColor = "#ffffff"
+  const buttonBackgroundColor = "#346df1"
+  const buttonBorderColor = "#346df1"
+  const buttonTextColor = "#ffffff"
+
+  return `
+<body style="background: ${backgroundColor};">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        <strong>Bunni Luna</strong>
+      </td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        Sign in as <strong>${email}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+`
+}
+
+function text({ url, host }) {
+  return `Sign in to ${host}\n${url}\n\n`
+}
 
 export default NextAuth({
     adapter: MongoDBAdapter(clientPromise),
@@ -17,7 +81,8 @@ export default NextAuth({
                 pass: process.env.EMAIL_SERVER_PASSWORD
               }
             },
-            from: process.env.EMAIL_FROM
+            from: process.env.EMAIL_FROM,
+            sendVerificationRequest: customVerificationRequest
           }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
@@ -56,4 +121,3 @@ export default NextAuth({
         }
     }
 })
-
