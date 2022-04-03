c22279fef4167eb13ae9e91c96ca51b3f487e431
David Medero <dmedero@getwrecked.com>
2022-04-02 22:47:46 -0400

    styling mobile menu

--- components/MobileMenuModal.js
+++ components/MobileMenuModal.js
@@ -2,7 +2,8 @@ import { useRef, Fragment, useState } from "react"
 import { Dialog, Transition } from '@headlessui/react'
 import { useSwipeable } from 'react-swipeable'
 import { XIcon } from '@heroicons/react/outline'
-
+import collections from '../categories'
+import Link from 'next/link'
 
 export default function MobileMenuModal({ show, onClose }) {
   const cancelButtonRef = useRef()
@@ -35,19 +36,19 @@ export default function MobileMenuModal({ show, onClose }) {
             >
                 <Dialog.Overlay {...handlers} className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
             </Transition.Child>
-            <div {...handlers} className="fixed inset-y-0 right-0 sm:pl-10 max-w-full flex">
+            <div {...handlers} className="fixed inset-y-0 left-0 max-w-full flex">
                 <Transition.Child
                 {...handlers}
                 as={Fragment}
                 enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
+                enterFrom="-translate-x-full"
+                enterTo="-translate-x-0"
                 leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
+                leaveFrom="-translate-x-0"
+                leaveTo="-translate-x-full"
                 >
-                <div {...handlers} className="w-screen max-w-full sm:max-w-md">
-                    <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
+                <div {...handlers} className="w-screen max-w-full sm:max-w-md overflow-y-scroll">
+                    <div className="h-full flex flex-col bg-white shadow-xl">
                     <div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
                         <div className="flex items-start justify-between">
                         <Dialog.Title className="text-lg font-medium text-gray-900">Menu</Dialog.Title>
@@ -64,9 +65,21 @@ export default function MobileMenuModal({ show, onClose }) {
                         </div>
                         </div>
                         <div {...handlers} className="mt-8">
-                        <div className="flow-root">
-                            Category
+                        <div>
+                            <div className="flex flex-col">
+                                {
+                                    collections.map(collection => (
+                                        <Link href={'/' + collection.handle} >
+                                            <a 
+                                            className="py-6 hover:bg-pink-100 font-semibold"
+                                            onClick={onClose}>
+                                                {collection.title}
+                                            </a>
+                                        </Link>
+                                    ))
+                                }
                             </div>
+                        </div>
                         </div>
                     </div>
                     </div>
