d4d80f7577c05d332bd48ab2e289af16c489c383
David Medero <dmedero@getwrecked.com>
2022-05-16 22:45:59 -0400

    styling

--- components/Nav.js
+++ components/Nav.js
@@ -178,7 +178,7 @@ export default function Nav() {
             </div>
             <div className='flex items-center relative'>
               <div 
-              className='relative right-5'
+              className='relative right-5 hover:scale-[1.3] transition-all duration-200 ease-in-out'
               ref={ref}
               onClick={() => {
                 toggleMenu();
@@ -193,10 +193,10 @@ export default function Nav() {
             className='cursor-pointer flex justify-center items-center'
             onClick={() => setCartOpen(!cartOpen)}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke={cartQuantity > 0 ? '#ff00a7' : "currentColor"} stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 hover:scale-[1.2] transition-all duration-200 ease-in-out" fill="none" viewBox="0 0 24 24" stroke={cartQuantity > 0 ? '#ff00a7' : "currentColor"} stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                 </svg> 
-                <span className='absolute top-[12px] text-[12px] text-[#ff00a7] font-semibold select-none'>{cartQuantity > 0 ? cartQuantity : ''}</span>
+                <span className='absolute z-[-1] top-[12px] text-[12px] text-[#ff00a7] font-semibold select-none'>{cartQuantity > 0 ? cartQuantity : ''}</span>
             </a>
             <Cart cart={cart} />
             <CurrencyConversion />
--- components/SignInButton.js
+++ components/SignInButton.js
@@ -13,7 +13,7 @@ export default function SignInButton() {
     <div>
       {!session && (
         <>
-        <button onClick={() => setShowModal(true)} className='highlight-removal flex items-center mr-5' type="button" data-modal-toggle="authentication-modal">
+        <button onClick={() => setShowModal(true)} className='highlight-removal flex items-center mr-5 hover:scale-[1.3] transition-all duration-200 ease-in-out' type="button" data-modal-toggle="authentication-modal">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="black" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
         </svg>
@@ -24,7 +24,7 @@ export default function SignInButton() {
         <>
       <Link href="/yourOrders" passHref>
       <a className='cursor-pointer'>
-      <button className='highlight-removal flex items-center mr-5' type="button">
+      <button className='highlight-removal flex items-center mr-5 hover:scale-[1.3] transition-all duration-200 ease-in-out' type="button">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="black" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
       </svg>
