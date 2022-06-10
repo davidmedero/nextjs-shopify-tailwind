d48b81a6b8a09affcf0f4d2f689d6644169a60c5
Yaima Pimienta <bunni@Yaimas-MacBook-Air.local>
2022-06-09 20:18:28 -0400

    debugging

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -7,7 +7,7 @@ export default function wishlist({ data, products }) {
 
   return (
     <div className="flex flex-col w-[1000px] max-w-full mx-auto xxs:px-6 xs:!px-8 text-white">
-        <div className="text-3xl font-bold mt-6 mb-20">Your Wishlist</div>
+        <div className="text-3xl font-bold mt-6 mb-20">Your Wishlist.</div>
         <div className="xxs:-mx-4 sm:mx-0 grid grid-cols-2 gap-y-10 gap-x-[22px] sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 cursor-pointer">
           {
             products.map(product => (
