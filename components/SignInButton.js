1e655ddabcb904ba45b9343a88dc037ef6a0c700
Yaima Pimienta <bunni@Yaimas-MacBook-Air.local>
2022-07-11 22:56:27 -0400

    pushing small styling change for sign in button

--- components/SignInButton.js
+++ components/SignInButton.js
@@ -25,7 +25,7 @@ export default function SignInButton() {
       <Link href="/account-dashboard" passHref>
       <a className='cursor-pointer'>
       <button className='px-[6px] relative highlight-removal flex items-center lg:hover:scale-[1.3] transition-all duration-200 ease-in-out' type="button">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="white">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="#ff00a7" viewBox="0 0 24 24" stroke="#ff00a7">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
       </svg>
       </button>
