1b5863541b5855b77c05c29715ba111636e76a8d
David Medero <dmedero@getwrecked.com>
2022-07-15 12:00:40 -0400

    cleanup phase 1

---
+++ components/ImageModal.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation, Pagination } from 'swiper'
+import { useEffect, useState, useCallback, useRef } from "react"
+
+
+export default function ImageModal() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
