5cf166ec675089eb9003d4c12b490bc119d0fa91
Yaima Pimienta <bunni@Yaimas-MacBook-Air.local>
2022-07-19 22:46:12 -0400

    styling

--- components/ImageModal.js
+++ components/ImageModal.js
@@ -400,18 +400,18 @@ export default function ImageModal({ show, onClose, product }) {
     };
   }, [handleDocumentMouseMove2, handleDocumentMouseUp2]);
 
-  useEffect(() => {
-    // First we get the viewport height and we multiply it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-  }, [scrollBoxTop2])
+  // useEffect(() => {
+  //   // First we get the viewport height and we multiply it by 1% to get a value for a vh unit
+  //   let vh = window.innerHeight * 0.01;
+  //   // Then we set the value in the --vh custom property to the root of the document
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  //   // We listen to the resize event
+  //   window.addEventListener('resize', () => {
+  //   // We execute the same script as before
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  //   });
+  // }, [scrollBoxTop2])
 
 
     return (
