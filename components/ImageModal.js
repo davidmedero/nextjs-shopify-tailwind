a2dfc1ae37f87bba47d4e6ecae2c5b4068032da3
Yaima Pimienta <bunni@Yaimas-MacBook-Air.local>
2022-07-19 23:21:43 -0400

    debugging closing bug

--- components/ImageModal.js
+++ components/ImageModal.js
@@ -411,7 +411,14 @@ export default function ImageModal({ show, onClose, product }) {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
-  }, [scrollBoxTop2])
+    return function cleanup() {
+      window.removeEventListener("resize", () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        });
+    };
+  }, [])
 
 
     return (
